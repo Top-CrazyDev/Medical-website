2b42d292122d4352fac90dccd035487b12c9a950
Top-CrazyDev <141586343+Top-CrazyDev@users.noreply.github.com>
2024-06-21 22:33:48 +0900

    Update Firebase.config.js

--- src/components/Login/Firebase/Firebase.config.js
+++ src/components/Login/Firebase/Firebase.config.js
@@ -7,7 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAtkocbe20ALsG6bGucsObY-mkPWrX17PY",
+  apiKey: "",
   authDomain: "healthcare-service-app.firebaseapp.com",
   projectId: "healthcare-service-app",
   storageBucket: "healthcare-service-app.appspot.com",
